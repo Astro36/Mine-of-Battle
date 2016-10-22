9ee579b5e3ea9fa40519b452834435fc318b1208
Astro36 <astr36@naver.com>
2016-10-22 18:05:36 +0900

    Fix item codes

--- main.js
+++ main.js
@@ -91,41 +91,41 @@ function init() {
         .setFunc((x, y, z, victim, user) => {
 
         }));
-    items.push(new ItemCompat(602, "Assassin II")
+    items.push(new ItemCompat(603, "Assassin II")
         .setCooldown(50)
         .setFunc((x, y, z, victim, user) => {
 
         }));
 
-    items.push(new ItemCompat(601, "Apollon I").setDamage(4));
-    items.push(new ItemCompat(602, "Apollon II")
+    items.push(new ItemCompat(604, "Apollon I").setDamage(4));
+    items.push(new ItemCompat(605, "Apollon II")
         .setDamage(4)
         .setFunc((x, y, z, victim, user) => {
 
         }));
 
-    items.push(new ItemCompat(600, "Barbarian I").setDamage(4));
-    items.push(new ItemCompat(601, "Barbarian II")
+    items.push(new ItemCompat(606, "Barbarian I").setDamage(4));
+    items.push(new ItemCompat(607, "Barbarian II")
         .setDamage(6)
         .setFunc((x, y, z, victim, user) => {
 
         }));
 
-    items.push(new ItemCompat(602, "Stance I"));
-    items.push(new ItemCompat(602, "Stance II"));
+    items.push(new ItemCompat(608, "Stance I"));
+    items.push(new ItemCompat(609, "Stance II"));
 
-    items.push(new ItemCompat(602, "Fighter I"));
-    items.push(new ItemCompat(602, "Fighter II")
+    items.push(new ItemCompat(610, "Fighter I"));
+    items.push(new ItemCompat(611, "Fighter II")
         .setCooldown(45)
         .setFunc((x, y, z, victim, user) => {
             Entity.setVelY(victim, 10);
         }));
 
-    items.push(new ItemCompat(602, "Phoenix I").setDamage(2)
+    items.push(new ItemCompat(612, "Phoenix I").setDamage(2)
         .setFunc((x, y, z, victim, user) => {
 
         }));
-    items.push(new ItemCompat(602, "Phoenix II").setDamage(3)
+    items.push(new ItemCompat(613, "Phoenix II").setDamage(3)
         .setFunc((x, y, z, victim, user) => {
             if (Player.isPlayer(victim)) {
                 Entity.setHealth(user, Entity.getHealth(user) + 1);
@@ -133,34 +133,34 @@ function init() {
 
         }));
 
-    items.push(new ItemCompat(602, "Fire Magicians I")
+    items.push(new ItemCompat(614, "Fire Magicians I")
         .setCooldown(30)
         .setFunc((x, y, z, victim, user) => {
 
         }));
-    items.push(new ItemCompat(602, "Fire Magicians II")
+    items.push(new ItemCompat(615, "Fire Magicians II")
         .setCooldown(50)
         .setFunc((x, y, z, victim, user) => {
 
         }));
 
-    items.push(new ItemCompat(602, "Ice Magicians I")
+    items.push(new ItemCompat(616, "Ice Magicians I")
         .setCooldown(30)
         .setFunc((x, y, z, victim, user) => {
 
         }));
-    items.push(new ItemCompat(602, "Ice Magicians II")
+    items.push(new ItemCompat(617, "Ice Magicians II")
         .setCooldown(25)
         .setFunc((x, y, z, victim, user) => {
 
         }));
 
-    items.push(new ItemCompat(602, "Riot Drinker I")
+    items.push(new ItemCompat(618, "Riot Drinker I")
         .setCooldown(30)
         .setFunc((x, y, z, victim, user) => {
 
         }));
-    items.push(new ItemCompat(602, "Riot Drinker II")
+    items.push(new ItemCompat(619, "Riot Drinker II")
         .setCooldown(45)
         .setFunc((x, y, z, victim, user) => {
 
