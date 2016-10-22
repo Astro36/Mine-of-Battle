8a488455951ebbba504eeb2f8a640408ac5f051a
Astro <astr36@naver.com>
2016-10-22 23:45:04 +0900

    Fix some bugs

--- main.js
+++ main.js
@@ -118,82 +118,82 @@ function init() {
             Enitity.addEffect(user, MobEffect.invisibility, 200, 0, false, true);
         }));
 
-    items.push(new ItemCompat(604, "Apollon I")));
-items.push(new ItemCompat(605, "Apollon II"));
+    items.push(new ItemCompat(604, "Apollon I"));
+    items.push(new ItemCompat(605, "Apollon II"));
 
-items.push(new ItemCompat(606, "Barbarian I").setDamage(4)
-    .setFunc((x, y, z, victim, user) => {
-        Enitity.addEffect(user, MobEffect.movementSpeed, 600, 1, false, true);
-    }));
-items.push(new ItemCompat(607, "Barbarian II")
-    .setDamage(200)
-    .setFunc((x, y, z, victim, user) => {
-        Enitity.addEffect(user, MobEffect.damageBoost, 200, 1, false, true);
-        Enitity.addEffect(user, MobEffect.damageResistance, 200, 1, false, true);
-        Enitity.addEffect(user, MobEffect.regeneration, 200, 1, false, true);
-        Enitity.addEffect(user, MobEffect.absorption, 200, 1, false, true);
-    }));
+    items.push(new ItemCompat(606, "Barbarian I").setDamage(4)
+        .setFunc((x, y, z, victim, user) => {
+            Enitity.addEffect(user, MobEffect.movementSpeed, 600, 1, false, true);
+        }));
+    items.push(new ItemCompat(607, "Barbarian II")
+        .setDamage(200)
+        .setFunc((x, y, z, victim, user) => {
+            Enitity.addEffect(user, MobEffect.damageBoost, 200, 1, false, true);
+            Enitity.addEffect(user, MobEffect.damageResistance, 200, 1, false, true);
+            Enitity.addEffect(user, MobEffect.regeneration, 200, 1, false, true);
+            Enitity.addEffect(user, MobEffect.absorption, 200, 1, false, true);
+        }));
 
-items.push(new ItemCompat(608, "Stance I"));
-items.push(new ItemCompat(609, "Stance II")
-    .setFunc((x, y, z, victim, user) => {
-        Enitity.addEffect(victim, MobEffect.poison, 200, 0, false, true);
-    }));
+    items.push(new ItemCompat(608, "Stance I"));
+    items.push(new ItemCompat(609, "Stance II")
+        .setFunc((x, y, z, victim, user) => {
+            Enitity.addEffect(victim, MobEffect.poison, 200, 0, false, true);
+        }));
 
-items.push(new ItemCompat(610, "Fighter I").setDamage(4));
-items.push(new ItemCompat(611, "Fighter II")
-    .setCooldown(45)
-    .setDamage(4)
-    .setFunc((x, y, z, victim, user) => {
-        Entity.setVelY(victim, 10);
-    }));
+    items.push(new ItemCompat(610, "Fighter I").setDamage(4));
+    items.push(new ItemCompat(611, "Fighter II")
+        .setCooldown(45)
+        .setDamage(4)
+        .setFunc((x, y, z, victim, user) => {
+            Entity.setVelY(victim, 10);
+        }));
 
-items.push(new ItemCompat(612, "Phoenix I").setDamage(2)
-    .setFunc((x, y, z, victim, user) => {
-        Enitity.addEffect(user, MobEffect.damageResistance, 600, 1, false, true);
-        Enitity.addEffect(user, MobEffect.regeneration, 600, 0, false, true);
-    }));
-items.push(new ItemCompat(613, "Phoenix II").setDamage(3)
-    .setFunc((x, y, z, victim, user) => {
-        if (Player.isPlayer(victim)) {
-            Entity.setFireTicks(victim, 1)
-            Entity.setHealth(user, Entity.getHealth(user) + 1);
-        }
+    items.push(new ItemCompat(612, "Phoenix I").setDamage(2)
+        .setFunc((x, y, z, victim, user) => {
+            Enitity.addEffect(user, MobEffect.damageResistance, 600, 1, false, true);
+            Enitity.addEffect(user, MobEffect.regeneration, 600, 0, false, true);
+        }));
+    items.push(new ItemCompat(613, "Phoenix II").setDamage(3)
+        .setFunc((x, y, z, victim, user) => {
+            if (Player.isPlayer(victim)) {
+                Entity.setFireTicks(victim, 1)
+                Entity.setHealth(user, Entity.getHealth(user) + 1);
+            }
 
-    }));
+        }));
 
-items.push(new ItemCompat(614, "Fire Magicians I")
-    .setCooldown(30)
-    .setFunc((x, y, z, victim, user) => {
+    items.push(new ItemCompat(614, "Fire Magicians I")
+        .setCooldown(30)
+        .setFunc((x, y, z, victim, user) => {
 
-    }));
-items.push(new ItemCompat(615, "Fire Magicians II")
-    .setCooldown(50)
-    .setFunc((x, y, z, victim, user) => {
+        }));
+    items.push(new ItemCompat(615, "Fire Magicians II")
+        .setCooldown(50)
+        .setFunc((x, y, z, victim, user) => {
 
-    }));
+        }));
 
-items.push(new ItemCompat(616, "Ice Magicians I")
-    .setCooldown(30)
-    .setFunc((x, y, z, victim, user) => {
+    items.push(new ItemCompat(616, "Ice Magicians I")
+        .setCooldown(30)
+        .setFunc((x, y, z, victim, user) => {
 
-    }));
-items.push(new ItemCompat(617, "Ice Magicians II")
-    .setCooldown(25)
-    .setFunc((x, y, z, victim, user) => {
+        }));
+    items.push(new ItemCompat(617, "Ice Magicians II")
+        .setCooldown(25)
+        .setFunc((x, y, z, victim, user) => {
 
-    }));
+        }));
 
-items.push(new ItemCompat(618, "Riot Drinker I")
-    .setCooldown(30)
-    .setFunc((x, y, z, victim, user) => {
+    items.push(new ItemCompat(618, "Riot Drinker I")
+        .setCooldown(30)
+        .setFunc((x, y, z, victim, user) => {
 
-    }));
-items.push(new ItemCompat(619, "Riot Drinker II")
-    .setCooldown(45)
-    .setFunc((x, y, z, victim, user) => {
+        }));
+    items.push(new ItemCompat(619, "Riot Drinker II")
+        .setCooldown(45)
+        .setFunc((x, y, z, victim, user) => {
 
-    }));
+        }));
 }
 
 init();
